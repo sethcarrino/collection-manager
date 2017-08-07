b4e41dda5fc92e8bfbbb062a47cb5a7bbdedb781
sethcarrino <sethcarrino@gmail.com>
2017-08-07 16:39:56 -0400

    accessing array

--- public/array.js
+++ public/array.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const qs = require('qs');
+const assert = require('assert');
+const routes = require('./routes/routes')
+
 const Collection = require('../models/collection');
